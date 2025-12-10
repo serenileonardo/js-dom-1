3e4f0a0c64c2c9b9e9d0d6a3e3fcdb4c069377ac
serenileonardo <serenileonardo.web@gmail.com>
2025-12-10 16:16:03 +0100

    feat: add img on script to turn on lamp

---
+++ script.js
@@ -0,0 +1,12 @@
+// const el = document.getElementById("outside");
+// el. AddEventListener("click , modifyText");
+
+const button = document.getElementById("accensione");
+console.log(button);
+
+
+button.addEventListener("click", () => {
+    lampadina.src = "img/yellow_lamp.png";
+})
+
+const img = document.getElementById("lampadina");
